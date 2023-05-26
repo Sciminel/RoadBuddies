b005d081c3210003bbc5e844378acf019b37ae40
Abdelkrim BARKAOUI <abdelkrim.barkaoui@acelys.local>
2023-05-26 11:10:11 +0200

    Premier commit, mise en place du projet spring boot

---
+++ src/main/java/com/roadbuddies/rbapi/RoadBuddiesApplication.java
@@ -0,0 +1,13 @@
+package com.roadbuddies.rbapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoadBuddiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoadBuddiesApplication.class, args);
+	}
+
+}
